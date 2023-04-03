1a0950b177e67f5a13e192a8d4ed61d977904f82
Zentnon <lethanhgiang20011@gmail.com>
2023-04-03 20:24:29 +0700

    first commit

--- app/src/main/java/com/example/mobile_labs01_20521260/MainActivity.java
+++ app/src/main/java/com/example/mobile_labs01_20521260/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.mobile_labs01_20521260;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
